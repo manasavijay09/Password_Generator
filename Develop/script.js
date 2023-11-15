c1472053d974036c8022f4dd20ec2d4c00369996
manasavijay09 <manasa.vijay09@gmail.com>
2023-11-15 10:43:20 -0600

    declared variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//declaring variables 
+var uppercaseChars="ABCDEFGHIJKLMNOPQRSTUVWX";
+ var lowercaseChars="abcdefghijklamnoqrstuvwxyz;"
+ var numberChars="0123456789";
+ var specialChars="!@#$%^&*()_+";
+ var input=[""] //declared to store user input
+ var randomPassword=" " // to store password
+
+ // Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -11,4 +24,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//generateBtn.addEventListener("click", writePassword);
